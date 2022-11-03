2867fe40ad1e9af7493aec040124cf73e2a8de8e
Giang98 <nguyenhoanggiang290998@gmail.com>
2022-11-03 07:27:33 +0700

    fix cors 03.11.22.07.27

--- src/index.js
+++ src/index.js
@@ -11,16 +11,7 @@ const app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    next();
-});
-app.use(cors({ origin: `${process.env.FRONT_END_BASE_URL}`, optionsSuccessStatus: 200, credentials: true }));
-app.options("*", cors({ origin: `${process.env.FRONT_END_BASE_URL}`, optionsSuccessStatus: 200 }));
-// app.use(cors());
+app.use(cors());
 
 initWebRoutes(app);
 
